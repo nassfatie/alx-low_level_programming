72ac0dca4aed5fab1669850c6a72594dda3f1803
nassfatie <nassfatie@gmail.com>
2022-09-09 15:29:26 -0700

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase.
- *
- * Return: Always 0 
- */
+*main - prints the alphabet in lowercase.
+*
+*Return: Always 0
+*/
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
