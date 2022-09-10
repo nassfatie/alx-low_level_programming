69729ff37b440ab695baf1792f9c9878da5aca7d
nassfatie <nassfatie@gmail.com>
2022-09-09 17:20:48 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
 /**
 *main - prints all single digit numbers of base 10 starting from 0
 *
-* Return: always 0
+*Return: always 0
 */
 int main(void)
 {
 	int i;
-	
-	for (i='0';i<='9';i++)
+
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
