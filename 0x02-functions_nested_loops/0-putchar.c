2d775ad241c2a0f055d4b638a19577d10f9ce754
nassfatie <nassfatie@gmail.com>
2022-09-13 16:03:10 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - entry point
+*
+*Return: Always
+*/
+int main(void)
+{
+        char *ch = "_putcahr";
+
+        while (*ch)
+        {
+                _putchar(*ch);
+                ch++;
+        }
+        _putchar('\n');
+        return (0);
+}
+
