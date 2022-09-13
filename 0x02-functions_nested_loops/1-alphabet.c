7dce79dd5287884637764d20e9cdc43250455575
nassfatie <nassfatie@gmail.com>
2022-09-13 16:55:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+return (0);
+}
