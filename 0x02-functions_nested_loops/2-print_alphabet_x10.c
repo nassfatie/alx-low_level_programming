21174a32fad87a599460b807ff7edf916ab16e70
nassfatie <nassfatie@gmail.com>
2022-09-13 18:01:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - entry point
+*
+* Return: always o
+*/
+void print_alphabet_x10(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
