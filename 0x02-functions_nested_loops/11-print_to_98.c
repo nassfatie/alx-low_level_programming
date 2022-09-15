1960779c9b4f1dc3917a3733d4d781155129b790
nassfatie <nassfatie@gmail.com>
2022-09-14 17:07:25 -0700

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,15 @@
 */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	for (n = 0; n < 98; n++)
 
 	{
-		printf("%d\n", n);
+		printf("%d, ", n);
 	}
+
+	for (n = 0; n > 98; n--)
+	{
+		printf("%d, ", n);
+	}
+	printf("%d\n", n);
 }
