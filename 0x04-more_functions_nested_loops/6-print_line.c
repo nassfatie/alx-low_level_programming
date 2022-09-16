7279c40fe69c3e0220a68b43e01a415a9321fc69
nassfatie <nassfatie@gmail.com>
2022-09-16 15:17:30 -0700

    print_lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_line - draws a straight line
+*@n: number of times the character _ should be printed
+*Return:
+*/
+void print_line(int n)
+{
+	int h;
+
+	for (h = n; h <= n; h++)
+
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
