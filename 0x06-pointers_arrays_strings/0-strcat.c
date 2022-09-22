45270252d9160b3aa8cd9d89a828119f84bcfee4
nassfatie <nassfatie@gmail.com>
2022-09-22 12:28:05 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest:destination.
+*@src:source.
+*
+*Return: destination
+*/
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i = 0;
+
+	while (*(dest + destlen) != '\0')
+	{
+	destlen++;
+	}
+
+	while (*(src + srclen) != '\0')
+	{
+		srclen++;
+	}
+
+	while (i < srclen)
+	{
+	*(dest + destlen) = *(src + i);
+	i++;
+	destlen++;
+	}
+	return (dest);
+}
