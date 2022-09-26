44e3252d957cf8a0cfbf53dba7b0082f11525918
nassfatie <nassfatie@gmail.com>
2022-09-26 16:51:55 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,11 +11,11 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k;
+	int i, j = 0, k = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		j += a[(size + i) * i];
+		j += a[(size + 1) * i];
 		k += a[(size - 1) * (i + 1)];
 	}
 
