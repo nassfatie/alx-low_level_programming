8f1b96c3a9263df7f44f725f187f08aedf0375fa
nassfatie <nassfatie@gmail.com>
2022-09-28 13:50:48 -0700

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*_pow_recursion - power of a number
+*@y:integer
+*@x:integer
+*
+*Return: success
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	return (-1);
+	if (y > 0)
+	return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
