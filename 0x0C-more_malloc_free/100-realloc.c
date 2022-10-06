e00560c9a0ad91c95d1fe3f9202c4ac1a509e6ec
nassfatie <nassfatie@gmail.com>
2022-10-06 15:41:29 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,7 +42,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	return (NULL);
 
 	for (i = 0; i < old_size && i < new_size; i++)
-	;
 	{
 	nptr[i] = ((char *) ptr)[i];
 	}
